2e0350dec08faf723690f21a39a8670ee3569c3f
Yan-Sena <25ygss@gmail.com>
2022-05-26 19:00:41 -0300

    Atividade php

---
+++ elementos_text.php
@@ -0,0 +1,8 @@
+<?php   
+   
+   echo "<h3>Recuperando os dados - Elementos Text</h3>";
+
+       echo "<b>Mensagem: </b>".$_POST['mensagem'];
+
+
+?>
---
+++ receber_dados.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Formulário - POST</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <style>
+
+
+        #Container {
+            padding: 2%;
+            text-align: left;
+            background-color: lightblue;
+            border: 3px solid black;
+            width: 100%;
+            height: 2000px;
+        }
+
+        button{
+
+            border: 2px solid black;
+            padding: 10px;
+            margin-left: 1.5%;
+
+        }
+
+        .cad{
+
+            margin: 1.5%;
+            padding: 1%;
+            border: 2px solid black;
+
+        }
+        
+
+    </style>
+
+</head>
+<body>
+
+
+<div id="Container">
+
+<div style="background-color: whitesmoke;" class="cad">
+<?php   
+   
+ $valor1 =  $_GET['val1'];
+ $valor2 =  $_GET['val2'];
+ 
+
+$resul = $valor1 * $valor2;
+       
+        
+       echo "<b>A área do retângulo é: {$resul}</b>";
+
+       $resul = ($valor1*2) + ($valor2*2);
+
+
+       echo "<br><b>O perimetro do retângulo é: {$resul}</b>";
+     
+
+?></div>
+
+    
+
+
+</div> 
+</body>
+</html>
